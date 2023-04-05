5c5641e009d8d52be5667924c5e0e6243b918b04
Jorge Panzera <panzera.jorge@gmail.com>
2023-04-05 20:53:02 -0300

    estructura basica 1

--- api/server.ts
+++ api/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
 import jwt from "jsonwebtoken";
 import { connection } from "./db";
@@ -50,6 +50,7 @@ const authenticateJWT = (req: Request, res: Response, next: any) => {
 app.post("/token", (req: Request, res: Response) => {
   const { username, password } = req.body;
 
+  // obtener username y password desde una bd
   if (username === "admin" && password === "password") {
     const token = generateToken({ username: "admin" }, "1h");
     res.json({ token });
@@ -67,7 +68,12 @@ app.get("/ping", (req: Request, res: Response) => {
 app.get("/pets", authenticateJWT, getAllPets);
 
 // Get a pet by id
-app.get("/pets/:id", authenticateJWT, getPetById);
+app.get("/pets/:id", authenticateJWT, async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    await getPetById(id)
+    }
+);
+
 
 // Create a new pet
 app.post("/pets", authenticateJWT, createPet);
@@ -83,4 +89,73 @@ app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
 
+
+// MEZCLAR ESTAS FUNCIONES Con las de cada metodo rest 
+/*
+// Gets a pet by id
+app.get("/pets/:id", logActivity, async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+  try {
+    const [rows] = await connection.query<Pet[]>("SELECT * FROM pets WHERE pet_id = ?", [id]);
+    if (rows.length === 1) {
+      res.send(rows[0]);
+    } else {
+      res.status(404).send({ message: "Pet not found" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: "Internal server error" });
+  }
+});
+
+// Adds a new pet
+app.post("/pets", logActivity, async (req: Request, res: Response) => {
+  const pet: Pet = req.body;
+  try {
+    const [result] = await connection.query("INSERT INTO pets SET ?", [pet]);
+    const petId = result.insertId;
+    pet.pet_id = petId;
+    res.send({ message: "Pet added successfully", pet });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: "Internal server error" });
+  }
+});
+
+// Updates a pet by id
+app.put("/pets/:id", logActivity, async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+  const updatedPet: Pet = req.body;
+  try {
+  const [result] = await connection.query("UPDATE pets SET ? WHERE pet_id = ?", [
+    updatedPet,
+    id,
+  ]);
+  if (result.affectedRows === 1) {
+    res.send({ message: "Pet updated successfully", pet: updatedPet });
+  } else {
+    res.status(404).send({ message: "Pet not found" });
+  }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: "Internal server error" });
+  }
+});
+
+// Deletes a pet by id
+app.delete("/pets/:id", logActivity, async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+  try {
+    const [result] = await connection.query("DELETE FROM pets WHERE pet_id = ?", [id]);
+    if (result.affectedRows === 1) {
+      res.send({ message: "Pet deleted successfully" });
+    } else {
+      res.status(404).send({ message: "Pet not found" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: "Internal server error" });
+  }
+});
+*/
 
