04275893195ef9ea6a1b7619c43c03ad58d462c6
Jorge <panzera.jorge@gmail.com>
2023-04-11 20:28:30 -0300

    fix verifyUser

--- api/app/services/users.services.ts
+++ api/app/services/users.services.ts
@@ -3,7 +3,7 @@ import db from '../db/users.db'
 const verifyUser =  async (username:string, password:string) : Promise<boolean> => {
 
     const user = await db.verifyUser(username,password)
-    if (user) {
+    if (user.length>0) {
         return(true)
     }
     return(false)
