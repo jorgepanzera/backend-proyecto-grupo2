00628c421d1c0752c772614e0422e8a4344e1d26
Jorge Panzera <panzera.jorge@gmail.com>
2023-05-03 17:57:11 -0300

    add error handling middleware

--- api/app/index.ts
+++ api/app/index.ts
@@ -8,9 +8,12 @@ import bodyParser from "body-parser"
 import { logActivity } from "./middleware/utils"
 import petRoutes from './routes/pet.routes.'
 import userRoutes from './routes/users.routes'
+import createError from 'http-errors'
 
 
 const app = express()
+
+// ** Middleware ** //
 
 // Middleware to parse JSON request bodies
 app.use(bodyParser.json());
@@ -19,15 +22,44 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Middleware para loguear actividad
 app.use(logActivity)
 
+// ** Routes ** //
+
 // Ping route to test API availability
 app.get("/ping", (req: Request, res: Response) => {
     res.send("Pong");
 });
-  
-// Routes
+
+// "Business" Routes
 app.use('/users', userRoutes)
 app.use('/pets', petRoutes)
 
 
+// ** Error Handling ** //
+
+app.use((req, res, next) => {
+    next(createError(404, 'Route not found'))
+})
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+    if (!error.status) {
+      error = createError(500, error);
+    }
+  
+    if (error.status >= 500) {
+      console.error(error);
+    }
+  
+    const data: any = {};
+    data.message = error.message;
+  
+    if (error.errors) {
+      data.errors = Object.keys(error.errors).reduce((errors: any, key) => {
+        errors[key] = error.errors[key].message;
+        return errors;
+      }, {});
+    }
+    
+    res.status(error.status).json(data);
+  });
 
 export default app
