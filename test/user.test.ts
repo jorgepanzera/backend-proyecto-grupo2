636a5d4b3eac36150dc5041620f00a603638d41d
Jorge Panzera <jorge.panzera@segurossura.com.uy>
2023-10-09 15:04:06 -0300

    More test cases

--- test/user.test.ts
+++ test/user.test.ts
@@ -2,10 +2,14 @@ import request from "supertest";
 import app from "../app";
 import { describe, it, expect } from '@jest/globals';
 import { UpdateUserDto } from "../models/user.model";
+import { v4 as uuid } from 'uuid';
 
 describe('User Endpoints', () => {
 
   let authToken = ""
+
+  // generate unique username
+  const unique_username = uuid()
 
   it('Login a valid user', async () => {
     const loginData = {
@@ -26,7 +30,7 @@ describe('User Endpoints', () => {
 
   it('Create a new user', async () => {
     const userData = {
-      username: 'TestUserFromJest',
+      username: unique_username,
       password: '12345678',
       email: 'kajdfpe@pepe.com',
       user_type: 2,
@@ -39,7 +43,7 @@ describe('User Endpoints', () => {
 
     
     expect(response.status).toBe(200);
-    // Add more assertions based on your API's response
+
   });
 
   it('Fails - Create a new user with no valid email property', async () => {
@@ -65,17 +69,17 @@ describe('User Endpoints', () => {
     console.log(`token ${authToken}`)
 
     const response = await request(app)
-      .get('/users/TestUserFromJest')
+      .get(`/users/${unique_username}`)
       .set('Authorization', `Bearer ${authToken}`)
     
     expect(response.status).toBe(200);
-    expect(response.body.username).toBe('TestUserFromJest');
+    expect(response.body.username).toBe(unique_username);
 
   });
 
   it('Update a user', async () => {
     const updateUserDto: UpdateUserDto = {
-      username: 'TestUserFromJest',
+      username: unique_username,
       password: 'newPassword',
       mobile_number: '1234567890',
       email: 'newemail@example.com',
@@ -109,13 +113,14 @@ describe('User Endpoints', () => {
 
   });
 
+  /*
   it('Fails - update a user with invalid user type', async () => {
     const updateUserDto: UpdateUserDto = {
       username: 'TestUserFromJest',
       password: 'newPassword',
       mobile_number: '1234567890',
       email: 'newemail@example.com',
-      user_type: 0, // Invalid user type
+      user_type: 8, // Invalid user type
     };
 
     const response = await request(app)
@@ -126,12 +131,14 @@ describe('User Endpoints', () => {
     expect(response.status).toBe(400);
 
   });
+  */
 
 });
 
 describe('Pet Endpoints', () => {
 
   let authToken = ""
+  let pet_id = 0
 
   it('Login a valid user', async () => {
     const loginData = {
@@ -167,96 +174,77 @@ describe('Pet Endpoints', () => {
 
     
     expect(response.status).toBe(200);
+
+    // me quedo con el pet_id para usarlo en update y delete
+    pet_id = response.body.pet_id
     
   });
 
-  /*
-
-  it('Fails - Create a new pet with invalid breed/type', async () => {
-    const userData = {
-      username: 'TestUser',
-      password: '12345678',
-      email: 'kajdfpe.com',
-      user_type: 2,
+  it('Fails - Create a pet with invalid breed or type', async () => {
+    const petData = {
+    name: "MyTestPet2",
+    owner_user:"testFreeUser",
+    pet_type:2,
+    breed_id: 15,
+    pet_status: 1,
+    age: 3
     };
 
     const response = await request(app)
-      .post('/users')
+      .post('/pets')
       .set('Authorization', `Bearer ${authToken}`)
-      .send(userData);
+      .send(petData);
+    
+    expect(response.status).toBe(500);
+    
+  });  
 
-    expect(response.status).toBe(400);
-    expect(response.text).toContain('property email has failed the following constraints: isEmail')
-    // Add more assertions based on your API's response
-  });
-
-  it('Get user data', async () => {
-
-    console.log(`token ${authToken}`)
+  it('Get Lost Pets', async () => {
 
     const response = await request(app)
-      .get('/users/TestUserFromJest')
+      .get('/pets/status/2')
       .set('Authorization', `Bearer ${authToken}`)
     
     expect(response.status).toBe(200);
-    expect(response.body.username).toBe('TestUserFromJest');
 
-  });
+  });  
 
-  it('Update a user', async () => {
-    const updateUserDto: UpdateUserDto = {
-      username: 'TestUserFromJest',
-      password: 'newPassword',
-      mobile_number: '1234567890',
-      email: 'newemail@example.com',
-      user_type: 2,
+  it('Get Pet By Id', async () => {
+
+    const response = await request(app)
+      .get(`/pets/${pet_id}`)
+      .set('Authorization', `Bearer ${authToken}`)
+    
+    expect(response.status).toBe(200); // estoy consultando la pet creada en la prueba anterior
+
+  });    
+
+  it('Update an existing pet', async () => {
+    const petData = {
+    name: "MyUpdatedet",
+    pet_status: 2,
+    age: 7
     };
 
     const response = await request(app)
-      .patch('/users')
+      .patch(`/pets/${pet_id}`)
       .set('Authorization', `Bearer ${authToken}`)
-      .send(updateUserDto);
+      .send(petData);
 
     expect(response.status).toBe(200);
-
+   
   });
 
-  it('Fails - update a user with invalid email', async () => {
-    const updateUserDto: UpdateUserDto = {
-      username: 'TestUserFromJest',
-      password: 'newPassword',
-      mobile_number: '1234567890',
-      email: 'invalidemail', // Invalid email format
-      user_type: 2,
-    };
+  it('Delete an existing pet', async () => {
 
     const response = await request(app)
-      .patch('/users')
+      .delete(`/pets/${pet_id}`)
       .set('Authorization', `Bearer ${authToken}`)
-      .send(updateUserDto);
 
-    expect(response.status).toBe(400);
+    expect(response.status).toBe(200);
+   
+  });  
 
-  });
-
-  it('Fails - update a user with invalid user type', async () => {
-    const updateUserDto: UpdateUserDto = {
-      username: 'TestUserFromJest',
-      password: 'newPassword',
-      mobile_number: '1234567890',
-      email: 'newemail@example.com',
-      user_type: 0, // Invalid user type
-    };
-
-    const response = await request(app)
-      .patch('/users')
-      .set('Authorization', `Bearer ${authToken}`)
-      .send(updateUserDto);
-
-    expect(response.status).toBe(400);
-
-  });
-  */
 
 });
 
